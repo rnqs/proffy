07addd58346f840a6b8515c4fec8bcd08ccdea0d
RenanArques <renanarques4448@gmail.com>
2020-08-05 11:13:28 -0300

    Create TeacherForm page layout and Input component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,47 @@
 import React from 'react'
 
+import Input from '../../components/Input'
 import Header from '../../components/Header'
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
 const TeacherForm: React.FC = () => {
   return (
     <div id="page-teacher-form" className="container">
-      <Header title="Que incrível que você quer dar aulas." />
+      <Header
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Metéria" />
+          <Input name="cost" label="Custo por hora de aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="submit">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   )
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Input from '../../components/Input'
 import Header from '../../components/Header'
 import TeacherItem from '../../components/TeacherItem'
 
@@ -10,20 +11,9 @@ const TeacherList: React.FC = () => {
     <div id="page-teacher-list">
       <Header title='Esses são os proffys disponíveis.'>
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matérias</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="subject" />
-          </div>
+          <Input name="subject" label="Matérias" />
+          <Input name="week-day" label="Dia da semana" />
+          <Input name="time" type="time" label="Hora" />
         </form>
       </Header>
 
